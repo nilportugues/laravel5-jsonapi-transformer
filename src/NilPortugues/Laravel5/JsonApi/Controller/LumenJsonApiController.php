629c131b1bc7802ea350586a5dae23c5ab589579
Chris Manson <chris@stonecircle.io>
2016-10-18 22:54:15 +0100

    fixing issues

--- src/NilPortugues/Laravel5/JsonApi/Controller/LumenJsonApiController.php
+++ src/NilPortugues/Laravel5/JsonApi/Controller/LumenJsonApiController.php
@@ -12,9 +12,6 @@ namespace NilPortugues\Laravel5\JsonApi\Controller;
 
 use Laravel\Lumen\Application;
 use Laravel\Lumen\Routing\Controller;
-
-use Illuminate\Container\Container;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use NilPortugues\Api\JsonApi\Http\Factory\RequestFactory;
 use NilPortugues\Api\JsonApi\Http\Response\ResourceNotFound;
@@ -51,115 +48,115 @@ abstract class LumenJsonApiController extends Controller
         }
     }
 
+    /**
+     * Get many resources.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        $apiRequest = RequestFactory::create();
 
-     /**
-      * Get many resources.
-      *
-      * @return \Symfony\Component\HttpFoundation\Response
-      */
-     public function index()
-     {
-         $apiRequest = RequestFactory::create();
+        $page = $apiRequest->getPage();
+        if (!$page->size()) {
+            $page->setSize($this->pageSize);
+        }
 
-         $page = $apiRequest->getPage();
-         if (!$page->size()) {
-             $page->setSize($this->pageSize);
-         }
+        $fields = $apiRequest->getFields();
+        $sorting = $apiRequest->getSort();
+        $included = $apiRequest->getIncludedRelationships();
+        $filters = $apiRequest->getFilters();
 
-         $fields = $apiRequest->getFields();
-         $sorting = $apiRequest->getSort();
-         $included = $apiRequest->getIncludedRelationships();
-         $filters = $apiRequest->getFilters();
+        $resource = new ListResource($this->serializer, $page, $fields, $sorting, $included, $filters);
 
-         $resource = new ListResource($this->serializer, $page, $fields, $sorting, $included, $filters);
+        $totalAmount = $this->totalAmountResourceCallable();
+        $results = $this->listResourceCallable();
 
-         $totalAmount = $this->totalAmountResourceCallable();
-         $results = $this->listResourceCallable();
+        $controllerAction = '\\'.get_called_class().'@index';
+        $uri = $this->uriGenerator($controllerAction);
 
-         $controllerAction = '\\'.get_called_class().'@index';
-         $uri = $this->uriGenerator($controllerAction);
+        return $this->addHeaders($resource->get($totalAmount, $results, $uri, get_class($this->getDataModel())));
+    }
 
-         return $this->addHeaders($resource->get($totalAmount, $results, $uri, get_class($this->getDataModel())));
-     }
+    /**
+     * Get single resource.
+     *
+     * @param $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show($id)
+    {
+        $apiRequest = RequestFactory::create();
 
-     /**
-      * Get single resource.
-      *
-      * @param $id
-      *
-      * @return \Symfony\Component\HttpFoundation\Response
-      */
-     public function show($id)
-     {
-         $apiRequest = RequestFactory::create();
+        $resource = new GetResource(
+            $this->serializer,
+            $apiRequest->getFields(),
+            $apiRequest->getIncludedRelationships()
+        );
 
-         $resource = new GetResource(
-             $this->serializer,
-             $apiRequest->getFields(),
-             $apiRequest->getIncludedRelationships()
-         );
+        $find = $this->findResourceCallable($id);
 
-         $find = $this->findResourceCallable($id);
+        return $this->addHeaders($resource->get($id, get_class($this->getDataModel()), $find));
+    }
 
-         return $this->addHeaders($resource->get($id, get_class($this->getDataModel()), $find));
-     }
+    /**
+     * @return ResourceNotFound
+     */
+    public function create()
+    {
+        return new ResourceNotFound();
+    }
 
-     /**
-      * @return ResourceNotFound
-      */
-     public function create()
-     {
-         return new ResourceNotFound();
-     }
+    /**
+     * Post Action.
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+        $createResource = $this->createResourceCallable();
+        $resource = new CreateResource($this->serializer);
 
-     /**
-      * Post Action.
-      *
-      * @param Request $request
-      *
-      * @return \Symfony\Component\HttpFoundation\Response
-      */
-     public function store(Request $request)
-     {
-         $createResource = $this->createResourceCallable();
-         $resource = new CreateResource($this->serializer);
-         return $this->addHeaders(
-           $resource->get((array) $request->get('data'), get_class($this->getDataModel()), $createResource)
-         );
-     }
+        return $this->addHeaders(
+            $resource->get((array) $request->get('data'), get_class($this->getDataModel()), $createResource)
+        );
+    }
 
-     /**
-      * @param $id
-      *
-      * @return Response
-      */
-     public function update(Request $request, $id)
-     {
-         return (strtoupper($request->getMethod()) === 'PUT') ? $this->putAction($request,
-             $id) : $this->patchAction($request, $id);
-     }
+    /**
+     * @param $id
+     *
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        return (strtoupper($request->getMethod()) === 'PUT') ?
+          $this->putAction($request, $id) : $this->patchAction($request, $id);
+    }
 
-     /**
-      * @return ResourceNotFound
-      */
-     public function edit()
-     {
-         return new ResourceNotFound();
-     }
+    /**
+     * @return ResourceNotFound
+     */
+    public function edit()
+    {
+        return new ResourceNotFound();
+    }
 
-     /**
-      * @param $id
-      *
-      * @return Response
-      */
-     public function destroy($id)
-     {
-         $find = $this->findResourceCallable($id);
+    /**
+     * @param $id
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $find = $this->findResourceCallable($id);
 
-         $delete = $this->deleteResourceCallable($id);
+        $delete = $this->deleteResourceCallable($id);
 
-         $resource = new DeleteResource($this->serializer);
+        $resource = new DeleteResource($this->serializer);
 
-         return $this->addHeaders($resource->get($id, get_class($this->getDataModel()), $find, $delete));
-     }
+        return $this->addHeaders($resource->get($id, get_class($this->getDataModel()), $find, $delete));
+    }
 }
